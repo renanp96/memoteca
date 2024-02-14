6f5d46e5c1d14832a33d9ad6907d68a33abbcbe7
renan.paganini <renan.paganini@fortesec.com.br>
2024-02-14 16:41:07 -0300

    feature: aplicado routers(rotas) para eventos de click dos botoes e inicializacao do form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+
+const routes: Routes = [
+  {
+  path: 'criarPensamento',
+  component: CriarPensamentoComponent
+  },
+
+  {
+    path: 'listarPensamento',
+    component: ListarPensamentoComponent
+  },
+  {
+    path: '',
+    redirectTo: 'listarPensamento',
+    pathMatch: 'full',
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
