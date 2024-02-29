de2c4f66d8101a47b1247cd1c7df1cf71e0575f0
renan.paganini <renan.paganini@fortesec.com.br>
2024-02-29 09:59:33 -0300

    feature: cridas regras de validacao de campos em branco ou com caracters insuficientes para criar um card

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -22,19 +22,37 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['Form Reativo'],
-      autoria: ['Angular'],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ])],
       modelo: ['modelo1']
     });
   }
 
   criarPensamento(){
-    this.service.criar(this.formulario.value).subscribe(() => {
-      this.router.navigate(['/listarPensamento']);
-    });
+    console.log("log: ", this.formulario.get('autoria')?.errors);
+
+    if(this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento']);
+      });
+    }
   }
 
   cancelarPensamento(){
     this.router.navigate(['/listarPensamento']);
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid){
+      return 'botao';
+    } else {
+      return 'botao__desabilitado';
+    }
   }
 }
